cec1010557e0c510d34e7b7e10fdb279e54d3d8c
Mohd Naushad Ahmed <mahmed@sierrawireless.com>
2024-12-06 17:32:41 +0530

    Updated

--- smtc/script/client.sh
+++ smtc/script/client.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-sudo ./../build/smtc --role client --peer-host 10.203.77.36 --peer-port 48989 --userid user --password Pin@411048
+sudo ./../build/smtc --role client --peer-host=10.203.77.36 --user=user --password=Pin@411048
